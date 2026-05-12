88d471fd659f9176eb870fa0444d5d1e031b91ca
kenanqafarov <qafarovkenan06@gmail.com>
2026-05-12 17:21:52 +0400

    chore: init project structure, add pom.xml and config files

---
+++ src/main/java/com/examme/examme/ExammeApplication.java
@@ -0,0 +1,13 @@
+package com.examme.examme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExammeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExammeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examme/examme/ExammeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examme.examme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExammeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
